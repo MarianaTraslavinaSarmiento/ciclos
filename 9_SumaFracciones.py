a4232c7242cd55293ccf97e0d51b3339da836465
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-01-20 12:25:03 -0500

    Solucion_Taller3

---
+++ 9_SumaFracciones.py
@@ -0,0 +1,20 @@
+
+#Programa que permita trabajar con las potencias fraccionales entre dos
+
+fraccion = 1
+exponente = 1
+suma = 0
+print(f"Potencia    Fraccion     Suma ")
+
+while fraccion>0.000001:
+    fraccion = 1/2**exponente
+    suma +=fraccion
+    print(str(exponente).ljust(8), str(round(fraccion, 7)).center(10), str(round(suma, 7)).rjust(10))
+    exponente+=1
+
+
+
+
+
+
+
